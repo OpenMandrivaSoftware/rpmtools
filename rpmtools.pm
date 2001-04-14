c37e49d43f3f6b12c9550e66ec7e471928583d5a
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-04-14 14:23:46 +0000

    fixed absurd mismatch of use <=> and cmp.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/rpmtools/trunk@11820 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- rpmtools.pm
+++ rpmtools.pm
@@ -539,7 +539,7 @@ sub version_compare {
 
     while ($a || $b) {
 	my ($sb, $sa) =  map { $1 if $a =~ /^\W*\d/ ? s/^\W*0*(\d+)// : s/^\W*(\D*)// } ($b, $a);
-	$_ = length($sa) cmp length($sb) || $sa cmp $sb and return $_ || 0;
+	$_ = length($sa) <=> length($sb) || $sa cmp $sb and return $_ || 0;
 	$sa eq '' && $sb eq '' and return $a cmp $b || 0;
     }
     0;
