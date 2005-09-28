7a77f5b72b75a6205c5392a9639c168ef33c0d28
rgarciasuarez <rgarciasuarez@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-09-28 09:54:36 +0000

    Remove obsolete code
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/rpmtools/trunk@12103 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- rpm2header.c
+++ rpm2header.c
@@ -6,102 +6,82 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <rpmlib.h>
-
-#ifdef RPM_42
 #include <rpm/rpmts.h>
-#endif
 
 #define FILENAME_TAG 1000000
 #define FILESIZE_TAG 1000001
 
 const char *basename(const char *f) {
-  char *p = strrchr(f, '/');
-  return p ? p + 1 : f;
+    char *p = strrchr(f, '/');
+    return p ? p + 1 : f;
 }
 
 int_32 FD_size(FD_t fd) {
-  struct stat sb;
-  fstat(fdFileno(fd), &sb);
-  return sb.st_size;
+    struct stat sb;
+    fstat(fdFileno(fd), &sb);
+    return sb.st_size;
 }
 
 int main(int argc, char **argv) {
-  int i;
-  FD_t fout;
+    int i;
+    FD_t fout;
 
-  if (argc < 2) {
-    fprintf(stderr, "usage: rpm2header <rpms>\n");
-    exit(1);
-  }
-
-  fout = fdDup(1 /*stdout*/);
-  
-  for (i = 1; i < argc; i++) {
-    FD_t fd;
-    Header h;
-    int_32 size;
-    const char *name = basename(argv[i]);
-#ifdef RPM_42
-    rpmts ts;
-    /* rpmVSFlags vsflags, ovsflags; */
-#else
-    int isSource;
-#endif
-    
-    fprintf(stderr, "%s\n", argv[i]);
-    
-    if (!(fd = fdOpen(argv[i], O_RDONLY, 0666))) {
-      perror("open");
-      exit(1);
+    if (argc < 2) {
+	fprintf(stderr, "usage: rpm2header <rpms>\n");
+	exit(1);
     }
-    size = FD_size(fd);
 
-#ifdef RPM_42
-    ts = rpmtsCreate();
-    rpmtsSetVSFlags(ts, _RPMVSF_NOSIGNATURES);
-    if (rpmReadPackageFile(ts, fd, argv[1], &h) == 0) {
-#else
-    if (rpmReadPackageHeader(fd, &h, &isSource, NULL, NULL) == 0) {
-#endif
-      headerRemoveEntry(h, RPMTAG_POSTIN);
-      headerRemoveEntry(h, RPMTAG_POSTUN);
-      headerRemoveEntry(h, RPMTAG_PREIN);
-      headerRemoveEntry(h, RPMTAG_PREUN);
-      headerRemoveEntry(h, RPMTAG_FILEUSERNAME);
-      headerRemoveEntry(h, RPMTAG_FILEGROUPNAME);
-      headerRemoveEntry(h, RPMTAG_FILEVERIFYFLAGS);
-      headerRemoveEntry(h, RPMTAG_FILERDEVS);
-      headerRemoveEntry(h, RPMTAG_FILEMTIMES);
-      headerRemoveEntry(h, RPMTAG_FILEDEVICES);
-      headerRemoveEntry(h, RPMTAG_FILEINODES);
-      headerRemoveEntry(h, RPMTAG_TRIGGERSCRIPTS);
-      headerRemoveEntry(h, RPMTAG_TRIGGERVERSION);
-      headerRemoveEntry(h, RPMTAG_TRIGGERFLAGS);
-      headerRemoveEntry(h, RPMTAG_TRIGGERNAME);
-      headerRemoveEntry(h, RPMTAG_CHANGELOGTIME);
-      headerRemoveEntry(h, RPMTAG_CHANGELOGNAME);
-      headerRemoveEntry(h, RPMTAG_CHANGELOGTEXT);
-      headerRemoveEntry(h, RPMTAG_ICON);
-      headerRemoveEntry(h, RPMTAG_GIF);
-      headerRemoveEntry(h, RPMTAG_VENDOR);
-      headerRemoveEntry(h, RPMTAG_EXCLUDE);
-      headerRemoveEntry(h, RPMTAG_EXCLUSIVE);
-      headerRemoveEntry(h, RPMTAG_DISTRIBUTION);
-      headerRemoveEntry(h, RPMTAG_VERIFYSCRIPT);
+    fout = fdDup(1 /*stdout*/);
 
-      /* removing that break updates.
-      headerRemoveEntry(h, RPMTAG_OLDFILENAMES);
-      headerRemoveEntry(h, RPMTAG_BASENAMES);
-      headerRemoveEntry(h, RPMTAG_DIRINDEXES);
-      headerRemoveEntry(h, RPMTAG_DIRNAMES);
-      */
+    for (i = 1; i < argc; i++) {
+	FD_t fd;
+	Header h;
+	int_32 size;
+	const char *name = basename(argv[i]);
+	rpmts ts;
 
-      headerAddEntry(h, FILENAME_TAG, RPM_STRING_TYPE, name, 1);
-      headerAddEntry(h, FILESIZE_TAG, RPM_INT32_TYPE, &size, 1);
-      headerWrite(fout, h, HEADER_MAGIC_YES);
-      headerFree(h);
+	fprintf(stderr, "%s\n", argv[i]);
+
+	if (!(fd = fdOpen(argv[i], O_RDONLY, 0666))) {
+	    perror("open");
+	    exit(1);
+	}
+	size = FD_size(fd);
+
+	ts = rpmtsCreate();
+	rpmtsSetVSFlags(ts, _RPMVSF_NOSIGNATURES);
+	if (rpmReadPackageFile(ts, fd, argv[1], &h) == 0) {
+	    headerRemoveEntry(h, RPMTAG_POSTIN);
+	    headerRemoveEntry(h, RPMTAG_POSTUN);
+	    headerRemoveEntry(h, RPMTAG_PREIN);
+	    headerRemoveEntry(h, RPMTAG_PREUN);
+	    headerRemoveEntry(h, RPMTAG_FILEUSERNAME);
+	    headerRemoveEntry(h, RPMTAG_FILEGROUPNAME);
+	    headerRemoveEntry(h, RPMTAG_FILEVERIFYFLAGS);
+	    headerRemoveEntry(h, RPMTAG_FILERDEVS);
+	    headerRemoveEntry(h, RPMTAG_FILEMTIMES);
+	    headerRemoveEntry(h, RPMTAG_FILEDEVICES);
+	    headerRemoveEntry(h, RPMTAG_FILEINODES);
+	    headerRemoveEntry(h, RPMTAG_TRIGGERSCRIPTS);
+	    headerRemoveEntry(h, RPMTAG_TRIGGERVERSION);
+	    headerRemoveEntry(h, RPMTAG_TRIGGERFLAGS);
+	    headerRemoveEntry(h, RPMTAG_TRIGGERNAME);
+	    headerRemoveEntry(h, RPMTAG_CHANGELOGTIME);
+	    headerRemoveEntry(h, RPMTAG_CHANGELOGNAME);
+	    headerRemoveEntry(h, RPMTAG_CHANGELOGTEXT);
+	    headerRemoveEntry(h, RPMTAG_ICON);
+	    headerRemoveEntry(h, RPMTAG_GIF);
+	    headerRemoveEntry(h, RPMTAG_VENDOR);
+	    headerRemoveEntry(h, RPMTAG_EXCLUDE);
+	    headerRemoveEntry(h, RPMTAG_EXCLUSIVE);
+	    headerRemoveEntry(h, RPMTAG_DISTRIBUTION);
+	    headerRemoveEntry(h, RPMTAG_VERIFYSCRIPT);
+	    headerAddEntry(h, FILENAME_TAG, RPM_STRING_TYPE, name, 1);
+	    headerAddEntry(h, FILESIZE_TAG, RPM_INT32_TYPE, &size, 1);
+	    headerWrite(fout, h, HEADER_MAGIC_YES);
+	    headerFree(h);
+	}
+	fdClose(fd);
     }
-    fdClose(fd);
-  }
-  return 0;
+    return 0;
 }
