901284ccff942c2fd2b3f236d5d0b39a1f8af4f2
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-07-18 13:43:12 +0000

    fix version detection
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/rpmtools/trunk@243518 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- parsehdlist.c
+++ parsehdlist.c
@@ -12,10 +12,12 @@
 #include <string.h>
 #include <ctype.h>
 
-#include <rpm/rpmlib.h>
-#if defined(RPM_FORMAT_VERSION) && RPM_FORMAT_VERSION >= 5
+#define RPM_VERSION(maj,min,pl) (((maj) << 16) + ((min) << 8) + (pl))
+
+#if RPM_VERSION_CODE >= RPM_VERSION(5,0,0)
 #include <rpm/rpm4compat.h>
 #else
+#include <rpm/rpmlib.h>
 #include <rpm/header.h>
 #endif
 
